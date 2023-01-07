8b10539b39ddc0fc4f2c88b3ae2ac4ac8a8917ab
Syed Shah Meer Haider <60551634+CyBeR1337x@users.noreply.github.com>
2023-01-07 17:53:07 +0500

    Create Node.cs

---
+++ LinkedList/ConsoleApp1/Node.cs
@@ -0,0 +1,12 @@
+namespace ConsoleApp1 {
+    class Node {
+
+        public int data;
+        public Node? next;
+
+        public Node(int data) {
+            this.data = data;
+            next = null;
+        }
+    }
+}
